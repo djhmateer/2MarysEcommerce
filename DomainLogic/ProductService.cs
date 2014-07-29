a8e5f97f9d4c4713fd96c15a00ec11eee729cc72
Dave Mateer home <davemateer@gmail.com>
2014-07-29 06:39:54 +0100

    First commit

---
+++ DomainLogic/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Ploeh.Samples.Mary.ECommerce.Data.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ //: Ploeh.Samples.Mary.ECommerce.Domain.IProductService
+namespace Ploeh.Samples.Mary.ECommerce.Domain {
+    public partial class ProductService {
+        private readonly CommerceObjectContext objectContext;
+
+        public ProductService() {
+            this.objectContext = new CommerceObjectContext();
+        }
+
+        public IEnumerable<Product> GetFeaturedProducts(
+            bool isCustomerPreferred) {
+            var discount = isCustomerPreferred ? .95m : 1;
+            var products = (from p in this.objectContext
+                                .Products
+                            where p.IsFeatured
+                            select p).AsEnumerable();
+            return from p in products
+                   select new Product {
+                       ProductId = p.ProductId,
+                       Name = p.Name,
+                       Description = p.Description,
+                       IsFeatured = p.IsFeatured,
+                       UnitPrice = p.UnitPrice * discount
+                   };
+        }
+    }
+
+    public partial class ProductService : IDisposable {
+        #region IDisposable Members
+
+        public void Dispose() {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                this.objectContext.Dispose();
+            }
+        }
+    }
+}
---
+++ WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Ploeh.Samples.Mary.ECommerce.Data.Sql;
+using Ploeh.Samples.Mary.ECommerce.Domain;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            bool isPreferredCustomer = this.User.IsInRole("PreferredCustomer");
+
+            var service = new ProductService();
+            IEnumerable<Product> products = service.GetFeaturedProducts(isPreferredCustomer);
+            this.ViewData["Products"] = products;
+
+            return this.View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
